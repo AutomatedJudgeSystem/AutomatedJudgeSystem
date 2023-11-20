0116dfb6ec0618940b414c7ed4cbd0971b760c76
alliyah01 <124291804+alliyah01@users.noreply.github.com>
2023-11-20 09:10:18 -0400

    API documentation

--- src/main/java/automatedgrader/strategy/CalculationStrategy.java
+++ src/main/java/automatedgrader/strategy/CalculationStrategy.java
@@ -1,5 +1,14 @@
 package automatedgrader.strategy;
 
+/**
+ * Represents a calculation strategy for automated grading.
+ */
 public interface CalculationStrategy {
+    /**
+     * Calculates a score based on the content of a file.
+     *
+     * @param filePath The path to the file to be evaluated.
+     * @return The calculated score.
+     */
     int calculate(String filePath);
 }
