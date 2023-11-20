4f28fa30d8902ce37ade7558ba511bbece27b135
S-josiah <150189548+S-josiah@users.noreply.github.com>
2023-11-20 09:42:32 -0400

    Update SubmissionSubject.java

--- src/main/java/automatedgrader/observer/SubmissionSubject.java
+++ src/main/java/automatedgrader/observer/SubmissionSubject.java
@@ -1,5 +1,8 @@
 package automatedgrader.observer;
 
+import java.util.List;
+import automatedgrader.strategy.EvaluationResult;
+
 /**
  * The SubmissionSubject interface represents a subject that allows observers
  * to attach, detach, and be notified of changes.
