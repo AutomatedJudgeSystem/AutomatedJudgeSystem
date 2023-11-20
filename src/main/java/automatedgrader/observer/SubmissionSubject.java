d3735bc91b2a2b76d31cebc8b9ca68493482485c
alliyah01 <124291804+alliyah01@users.noreply.github.com>
2023-11-19 23:27:54 -0400

    API Documentation

--- src/main/java/automatedgrader/observer/SubmissionSubject.java
+++ src/main/java/automatedgrader/observer/SubmissionSubject.java
@@ -1,9 +1,28 @@
 package automatedgrader.observer;
 
-// Subject Interface
+/**
+ * The SubmissionSubject interface represents a subject that allows observers
+ * to attach, detach, and be notified of changes.
+ */
 public interface SubmissionSubject {
+
+  /**
+   * Attaches a PDFObserver to the subject.
+   *
+   * @param observer The PDFObserver to be attached.
+   */
   void attachObserver(PDFObserver observer);
+
+  /**
+   * Detaches a PDFObserver from the subject.
+   *
+   * @param observer The PDFObserver to be detached.
+   */
   void detachObserver(PDFObserver observer);
+
+  /**
+   * Notifies all attached PDFObservers of a change in the subject.
+   */
   void notifyObserver();
-} 
+}
 
