0b82f5ba46a074211288df35820d116a6fc4abe2
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-20 20:20:10 -0400

    Update EvaluationResult.java

--- src/main/java/automatedgrader/strategy/EvaluationResult.java
+++ src/main/java/automatedgrader/strategy/EvaluationResult.java
@@ -2,9 +2,15 @@ package automatedgrader.strategy;
 
 // Class representing the result of an evaluation
 public class EvaluationResult {
+     private String testname;
+     private String total;
+     private String feedback;
 
      public EvaluationResult( String testname, String total, String feedback ){
-
+          this.testname = testname;
+          this.total = total;
+          this.feedback = feedback;
+          
     }
     
     public String getTestName() {
