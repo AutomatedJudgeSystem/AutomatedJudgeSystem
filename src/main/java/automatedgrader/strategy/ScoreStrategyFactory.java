3c6c5447e612946ef1a03ebd326b86224f4f46d3
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-22 15:34:01 -0400

    Create ScoreStrategyFactory.java

---
+++ src/main/java/automatedgrader/strategy/ScoreStrategyFactory.java
@@ -0,0 +1,22 @@
+package projectv2.strategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreStrategyFactory {
+    private Map<String, ScoreCalculationStrategy> strategies;
+
+    public ScoreStrategyFactory(String studentFolderPath) {
+        strategies = new HashMap<>();
+        // Initialize strategies based on the classes or criteria you want to evaluate
+        strategies.put("Passenger", new PassengerScoreStrategy(studentFolderPath));
+        strategies.put("LuggageSlip", new LuggageSlipScoreStrategy(studentFolderPath));
+        strategies.put("LuggageManifest", new LuggageManifestScoreStrategy(studentFolderPath));
+        strategies.put("Flight", new FlightScoreStrategy(studentFolderPath));
+       // strategies.put("LuggageManagementSystem", new LuggageManagementSystemScoreStrategy(studentFolderPath));
+    }
+
+    public ScoreCalculationStrategy getStrategy(String className) {
+        return strategies.get(className);
+    }
+}
