776e7794581ab264242ddfa279eba3dfc0293edd
Christin120 <98425721+Christin120@users.noreply.github.com>
2023-11-22 17:52:56 -0400

    Update ScoreCalculationStrategy.java

--- src/main/java/automatedgrader/strategy/ScoreCalculationStrategy.java
+++ src/main/java/automatedgrader/strategy/ScoreCalculationStrategy.java
@@ -1,5 +1,16 @@
 package automatedgrader.strategy;
 
+/**
+ * The ScoreCalculationStrategy interface defines the contract for classes that perform
+ * score calculation based on specific rules or strategies.
+ */
 public interface ScoreCalculationStrategy {
+
+    /**
+     * Calculates and returns a score based on the implementation-specific rules or strategy.
+     *
+     * @return The calculated score.
+     */
     double calculateScore();
 }
+
